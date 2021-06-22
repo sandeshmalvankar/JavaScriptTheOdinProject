f11736d664e049a4621f5a18e814aa880b9ef60c
Sandesh Malvankar <sandesh.malvankar@indexnine.com>
2021-06-22 16:22:52 +0530

    filtered required data and printed it on console

--- Asynchronous JavaScript/WeatherApp/src/api.js
+++ Asynchronous JavaScript/WeatherApp/src/api.js
@@ -18,4 +18,56 @@ async function weatherApi(city) {
   }
 }
 
-export { validateInputCity, weatherApi };
+function getDate() {
+    let date = new Date();
+  
+    return (
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    );
+  }
+  
+  function getTime() {
+    let time = new Date();
+    return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+  }
+  
+  function parseTemp(temp) {
+    let currentUnit = 0; // 0 - Celsius 1 - Fahrenheit
+    let parsedTemp =
+      currentUnit === 0 ? Number(temp) - 273.1 : 1.8 * (Number(temp) - 273) + 32;
+    return Math.round(parsedTemp * 10) / 10;
+  }
+  
+  function filterData(data) {
+    let city = data.name;
+    let visibility = data.visibility;
+    let airPressure = data.pressure;
+    let windSpeed = data.wind.speed;
+  
+    let {
+      temp,
+      temp_max: tempMax,
+      temp_min: tempMin,
+      feels_like: temperatureFelt,
+      humidity,
+    } = data.main;
+  
+    let main = data.weather[0].main;
+  
+    return {
+      date: getDate(),
+      time: getTime(),
+      city,
+      visibility: Number(visibility) / 1000,
+      airPressure,
+      humidity,
+      windSpeed,
+      temp: parseTemp(temp),
+      tempMax: parseTemp(tempMax),
+      tempMin: parseTemp(tempMin),
+      temperatureFelt: parseTemp(temperatureFelt),
+    };
+  }
+  
+  
+export { validateInputCity, weatherApi, filterData };
--- Asynchronous JavaScript/WeatherApp/src/index.js
+++ Asynchronous JavaScript/WeatherApp/src/index.js
@@ -1,13 +1,13 @@
 import APIKEY from './apikey'
-import { validateInputCity, weatherApi } from './api'
+import { validateInputCity, weatherApi, filterData } from './api'
 
 //console.log(APIKEY)
-let inputCity = document.querySelector('#city')
+let inputCity = document.querySelector('#input-city')
 let searchBtn = document.querySelector('#search')
 
 searchBtn.onclick = () => { 
     if(validateInputCity(inputCity.value)){
         let weatherData = weatherApi(inputCity.value)
-        weatherData.then(data => console.log(data))
+        weatherData.then(data => console.log(filterData(data)))
     }
 }
