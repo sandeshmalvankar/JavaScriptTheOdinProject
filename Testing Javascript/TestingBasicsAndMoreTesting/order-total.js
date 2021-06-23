b7bbf71a2388be7debf18d6de6cf9d1ddb45ffa3
Sandesh Malvankar <sandesh.malvankar@indexnine.com>
2021-06-23 20:36:37 +0530

    jest test for order-total

---
+++ Testing Javascript/TestingBasicsAndMoreTesting/order-total.js
@@ -0,0 +1,10 @@
+const { apikey } = require("./apikey");
+
+function orderTotal(order) {
+  return order.items.reduce(
+    (prev, cur) => cur.price * (cur.quantity || 1) + prev,
+    0
+  );
+}
+
+module.exports = { orderTotal };
