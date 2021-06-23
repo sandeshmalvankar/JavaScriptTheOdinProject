bad6fcafc6efd2c66e6f4b5be72dd9252db6361e
Sandesh Malvankar <sandesh.malvankar@indexnine.com>
2021-06-23 20:42:38 +0530

    jest test of Object, Number, Strings, Arrays, Exception and Truthiness

---
+++ Testing Javascript/TestingBasicsAndMoreTesting/matchers-test.js
@@ -0,0 +1,107 @@
+const { test, expect } = require("@jest/globals");
+
+//use euqalTo if u want check value of an object
+test("Obj assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
+//----------------------------------------------------------------------------------------------------
+
+
+/*
+Truthiness
+toBeNull - matches only null
+toBeUndefined - matches only undefined
+toBeDefined - is the opposite of toBeUndefined
+toBeTruthy - matches anything that an if statement treats as true
+toBeFalsy - matches anything that an if statement treats as false
+*/
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+//----------------------------------------------------------------------------------------------------
+
+
+//Numbers (comparison)
+test("Compare number", () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+});
+// toBe and toEqual are equivalent for numbers
+
+//For floating point numbers use toBeCloseTo rather than toBe
+test("adding floating point numbers", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3); //rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+//----------------------------------------------------------------------------------------------------
+
+
+//Strings
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
+});
+//----------------------------------------------------------------------------------------------------
+
+
+//Arrays and iterables
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
+});
+//----------------------------------------------------------------------------------------------------
+
+
+//Exceptions
+function compileAndroidCode() {
+  throw new Error("you are using the wrong JDK");
+}
+
+test("compiling android goes as expected", () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
